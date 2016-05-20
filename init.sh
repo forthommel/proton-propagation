07d736fc6c287be8c23f92e73d4f06a7b2fe74a1
Laurent Forthomme <laurent.forthomme@cern.ch>
2016-05-20 14:47:41 +0200

    Added the update of the Hector submodule into the init.sh script ; ensuring the pythia source code is not already present when retrieving it

--- init.sh
+++ init.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
 PYTHIA_VERSION=6428
-curl "http://home.thep.lu.se/~torbjorn/pythia6/pythia${PYTHIA_VERSION}.f" > external/pythia${PYTHIA_VERSION}.f
+
+PYTHIA_DIR=external/
+PYTHIA_FILE=pythia${PYTHIA_VERSION}.f
+if [ ! -f ${PYTHIA_DIR}${PYTHIA_FILE} ]; then
+  curl "http://home.thep.lu.se/~torbjorn/pythia6/${PYTHIA_FILE}" > ${PYTHIA_DIR}${PYTHIA_FILE}
+fi
+
+# submodules initialisation
+git submodule update --init --recursive # fetch the latest version of xpedaq
+git submodule foreach git pull origin master
